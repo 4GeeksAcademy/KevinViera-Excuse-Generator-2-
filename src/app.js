c45c0b2a800c094db050d8701fcd1a5775637c54
KevinViera <kyohanviera@gmail.com>
2025-05-23 18:56:11 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+    document.querySelector('#Excuse').innerHTML = generateExcuse();
 };
+
+let generateExcuse = () => {
+
+    let noun = ['Kevin', 'A gigant', 'A spaceship', 'A dragon'];
+    let verb = ['destroyed', 'stepped on', 'blasted', 'kicked'];
+    let subject = ['my house', 'my phone', 'my car', 'my pc'];
+    let time = ['while I was sleeping', 'while I was having breakfast', 'while I was at the gym', 'while I was fishing outside'];
+    
+    let nindex = Math.floor(Math.random() * 4);
+    let vindex = Math.floor(Math.random() * 4);
+    let sindex = Math.floor(Math.random() * 4);
+    let tindex = Math.floor(Math.random() * 4);
+
+    return noun[nindex] + ' ' + verb[vindex] + ' ' + subject[sindex] + ' ' + time[tindex];
+}
